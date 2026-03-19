025588fc3b6d8866911327c16a5442878f984abc
achala2702 <achala2702@gmail.com>
2026-03-19 16:27:43 +0530

    put icons to buttons'

--- frontend/src/app/components/task-card/task-card.ts
+++ frontend/src/app/components/task-card/task-card.ts
@@ -3,10 +3,11 @@ import { MatButton } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { DropDown } from '../drop-down/drop-down';
 import { TaskModel } from '../../models/task-model';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-task-card',
-  imports: [MatCardModule, MatButton, DropDown],
+  imports: [MatCardModule, MatButton, DropDown, MatIconModule],
   templateUrl: './task-card.html',
   styleUrl: './task-card.scss',
 })
--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -3,10 +3,12 @@ import { Header } from '../../components/header/header';
 import { TaskCard } from '../../components/task-card/task-card';
 import { TaskService } from '../../services/task-service';
 import { TaskModel } from '../../models/task-model';
+import { MatAnchor, MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, TaskCard],
+  imports: [Header, TaskCard, MatAnchor, MatButtonModule, MatIconModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
